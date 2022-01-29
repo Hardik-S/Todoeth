07d9687264c19513d74ec7f65d13938ed7f58943
Hardik Shrestha <hardik.ho@gmail.com>
2022-01-29 11:04:18 -0500

    Project Setup

---
+++ contracts/TodoList.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.0;
+
+contract TodoList {
+	// similar to class variables in oop, these state variables represent the state of this smart contract and scopes the whole project
+	// public enables reading of this state variable from outside this contract
+	uint public taskCount = 0;
+
+
+}
