e01c5cf0f771e961ff7bfa3af3b8dcc6c99a2544
Hardik Shrestha <hardik.ho@gmail.com>
2022-01-29 21:17:41 -0500

    Setup index and app

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,8 +2,28 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
 	// similar to class variables in oop, these state variables represent the state of this smart contract and scopes the whole project
-	// public enables reading of this state variable from outside this contract
+	// public enables reading of this unsigned interger state variable from outside this contract
 	uint public taskCount = 0;
 
+	struct Task {
+		uint id;
+		string content;
+		bool completed;
+
+	}
+
+	// dynamic size key value pair (think expandable hashtable)
+	mapping(uint => Task) public tasks;
+
+	constructor() public {
+		createTask("Pet Rythm or Kimchi");
+	}
+	function createTask(string memory _content) public {
+		taskCount++;
+
+		// create a new task in the list `tasks`
+		tasks[taskCount] = Task(taskCount, _content, false);
+
+	}
 
 }
